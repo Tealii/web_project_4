5539136ebcd417d3ebb692ddbd5ff8d402c24875
Tealii <tea.gue@yahoo.com>
2020-05-22 14:09:30 -0500

    feat: added functionality to form style: edited styles in profile section dealing with   different viewports and input values refactor: renamed some styles

--- script.js
+++ script.js
@@ -4,10 +4,31 @@ const modal = document.querySelector(".modal");
 
 const closeBtn = modal.querySelector(".modal__exit-btn");
 
-editBtn.addEventListener("click", openCloseModal);
+const saveBtn = modal.querySelector(".modal__form-submit")
 
+const form = modal.querySelector(".modal__form");
+
+
+// opening/closing modal
+editBtn.addEventListener("click", openCloseModal);
 closeBtn.addEventListener("click", openCloseModal);
+saveBtn.addEventListener("click", openCloseModal);
 
 function openCloseModal() {
   modal.classList.toggle("display-modal");
+}
+
+// form handler
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(evt) {
+  evt.preventDefault();
+  let nameInput = modal.querySelector(".modal__form-name");
+  let aboutInput = modal.querySelector(".modal__form-about");
+
+  let name = document.querySelector(".profile-info__name");
+  let about = document.querySelector(".profile-info__about");
+
+  name.textContent = nameInput.value;
+  about.textContent = aboutInput.value;
 }
