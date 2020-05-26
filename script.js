855b895f8423e834527b08ef0b0de7b511cd1206
Tealii <tea.gue@yahoo.com>
2020-05-26 14:43:11 -0500

    RC unfinished pic modal

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 const editBtn = document.querySelector(".profile-info__edit-btn");
 
 const modal = document.querySelector(".modal");
+const profileModal = document.querySelector(".profile-modal");
+const figModal = document.querySelector(".fig-modal");
 
-const closeBtn = modal.querySelector(".modal__exit-btn");
+const closeBtn = document.querySelectorAll(".modal__exit-btn");
 
 const saveBtn = modal.querySelector(".modal__form-submit")
 
 const form = modal.querySelector(".modal__form");
 
+const nameInput = modal.querySelector(".modal__form-name");
+const aboutInput = modal.querySelector(".modal__form-about");
 
-// opening/closing modal
-function openCloseModal() {
-  modal.classList.toggle("modal_active");
+const name = document.querySelector(".profile-info__name");
+const about = document.querySelector(".profile-info__about");
+
+const pic = document.querySelectorAll(".photo-grid__item");
+
+
+// opening/closing modals
+function toggleProfileModal() {
+  profileModal.classList.toggle("modal_active");
 }
+
+  pic.forEach( pic => {
+    pic.addEventListener("click", (e) => {
+      figModal.classList.toggle("modal_active");
+      figModal.src = this.src;
+    });
+  });
+
+
+ function closeModal(){
+   if (figModal.classList.contains("modal_active") ||
+   profileModal.classList.contains("modal_active")) {
+     
+     figModal.classList.remove("modal_active");
+      profileModal.classList.remove("modal_active");
+   }
+ }
+
+
+closeBtn.forEach( closeBtn => {
+  closeBtn.addEventListener("click", closeModal);
+});
+
 
 // form handler
 function formSubmit(evt) {
   evt.preventDefault();
-  const nameInput = modal.querySelector(".modal__form-name");
-  const aboutInput = modal.querySelector(".modal__form-about");
-  
-  const name = document.querySelector(".profile-info__name");
-  const about = document.querySelector(".profile-info__about");
   
   name.textContent = nameInput.value;
   about.textContent = aboutInput.value;
@@ -29,7 +57,6 @@ function formSubmit(evt) {
 
 
 // events
-editBtn.addEventListener("click", openCloseModal);
-closeBtn.addEventListener("click", openCloseModal);
-saveBtn.addEventListener("click", openCloseModal);
+editBtn.addEventListener("click", toggleProfileModal);
+saveBtn.addEventListener("click", toggleProfileModal);
 form.addEventListener("submit", formSubmit);
