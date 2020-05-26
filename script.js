4ed8df6cd32fa14b47982ae190b42a949ad0eb4d
Tealii <tea.gue@yahoo.com>
2020-05-26 15:13:40 -0500

    feat: added create modal

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const editBtn = document.querySelector(".profile-info__edit-btn");
 
 const modal = document.querySelector(".modal");
 const profileModal = document.querySelector(".profile-modal");
 const figModal = document.querySelector(".fig-modal");
+const createModal = document.querySelector(".create-modal");
 
+const editBtn = document.querySelector(".profile-info__edit-btn");
 const closeBtn = document.querySelectorAll(".modal__exit-btn");
-
 const saveBtn = modal.querySelector(".modal__form-submit")
-
 const form = modal.querySelector(".modal__form");
+const addBtn = document.querySelector(".profile__add-button");
 
-const nameInput = modal.querySelector(".modal__form-name");
-const aboutInput = modal.querySelector(".modal__form-about");
+const nameInput = modal.querySelector(".modal__input_type_name");
+const aboutInput = modal.querySelector(".modal__input_type_about");
 
 const name = document.querySelector(".profile-info__name");
 const about = document.querySelector(".profile-info__about");
@@ -24,6 +24,10 @@ function toggleProfileModal() {
   profileModal.classList.toggle("modal_active");
 }
 
+function toggleCreateModal() {
+  createModal.classList.toggle("modal_active");
+}
+
   pic.forEach( pic => {
     pic.addEventListener("click", (e) => {
       figModal.classList.toggle("modal_active");
@@ -34,10 +38,12 @@ function toggleProfileModal() {
 
  function closeModal(){
    if (figModal.classList.contains("modal_active") ||
-   profileModal.classList.contains("modal_active")) {
+   profileModal.classList.contains("modal_active") ||
+   createModal.classList.contains("modal_active")) {
      
      figModal.classList.remove("modal_active");
       profileModal.classList.remove("modal_active");
+      createModal.classList.remove("modal_active");
    }
  }
 
@@ -60,3 +66,4 @@ function formSubmit(evt) {
 editBtn.addEventListener("click", toggleProfileModal);
 saveBtn.addEventListener("click", toggleProfileModal);
 form.addEventListener("submit", formSubmit);
+addBtn.addEventListener("click", toggleCreateModal);
